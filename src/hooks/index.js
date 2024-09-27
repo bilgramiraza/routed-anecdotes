a94cfb46717a548cd12bdd9038dbd828b5c2f9bd
Raza <bilgramiraza@yahoo.com>
2024-09-27 12:28:38 +0530

    Added Custom Hook for Form Fields

---
+++ src/hooks/index.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const useField = (name, type) => {
+	const [value, setValue] = useState('');
+
+	const onChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	return {
+		name,
+		type,
+		value,
+		onChange,
+	};
+};
