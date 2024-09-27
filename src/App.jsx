a2f410b3077d548016f4606f0aee966e64153bb6
Raza <bilgramiraza@yahoo.com>
2024-09-27 12:28:52 +0530

    Implemented Custom hook for Form Fields

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   useMatch,
   useNavigate,
 } from 'react-router-dom';
+import { useField } from './hooks';
 
 const Notification = ({ message }) => {
   if (!message) return null;
@@ -74,17 +75,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content', 'text');
+  const author = useField('author', 'text');
+  const info = useField('info', 'text');
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -95,15 +96,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
